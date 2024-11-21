f9349bdad96788d0ff13e16c0de6cc458f027fa0
Marshall Vogt <vogt_marshall@yahoo.com>
2024-11-21 05:44:26 +0000

    first commit

---
+++ Section-01/Project-01/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from Project-01" << endl;
+    return 0;
+}
---
+++ Section-01/Project-02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fav_number{100};
+    cout << "Hello from Project-02" << endl;
+    cout << "Enter your favorite number: ";
+    cin >> fav_number;
+    cout << "Your favorite number is " << fav_number << endl;
+    return 0;
+}
