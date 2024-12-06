53b7db6f397761c653b156ec8ae199e37ace988d
Marshall Vogt <vogt_marshall@yahoo.com>
2024-12-06 19:59:48 +0000

    Section 06

---
+++ Section-06/GlobalVariables/main.cpp
@@ -0,0 +1,17 @@
+// Section 6
+// Global and local variables 
+
+#include <iostream>
+ 
+using namespace std;
+
+ 
+int main() {
+    
+    int age {18};
+    
+    cout << age << endl;
+    
+    return 0;
+}
+
